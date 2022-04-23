cdae072fbac71774fb1b3f4e013a93d9199e4838
YuvarajAutomation <103816616+YuvarajAutomation@users.noreply.github.com>
2022-04-23 19:46:53 +0530

    Add files via upload
    Day 1 assignments

---
+++ Fibonacciseries.java
@@ -0,0 +1,51 @@
+package com.java.basic.day1.assignments;
+
+import java.util.Iterator;
+
+public class Fibonacciseries {
+	
+	/*
+	 * Goal: To find Fibonacci Series for a given range
+	 * 
+	 * input(range): 8 output: 0, 1, 1, 2, 3, 5, 8, 13
+	 * 
+	 * Shortcuts: 1) Print : type: syso, followed by: ctrl + space + enter 2) To
+	 * create a 'for' loop: type 'for', followed by ctrl + space + down arrow +
+	 * enter
+	 * 
+	 * What are my learnings from this code? 
+	 * 1) how to put range value and iterate in for loop
+	 *  
+	 * 2) learnt shortcuts of for loop statement and print statements
+	 * 
+	 * 
+	 * 3) learnt assign variables as per expectation
+	 * 
+	 */
+
+	public static void main(String[] args) {
+		// initialize 3 int variables (Tip: range = 8, firstNum = 0, secNum = 1, sum in the series)
+		int range=8, firstNum=0, secNum=1;
+		
+	int sum=0;
+
+				// Print first number
+		System.out.println(firstNum);
+		System.out.println(secNum);
+				// Iterate from 1 to the range
+				for (int i = 2; i < range; i++) {
+					// add first and second number assign to sum
+                    sum = firstNum+secNum;
+   				// Assign second number to the first number
+
+                    firstNum=secNum;
+                   
+   				// Assign sum to the second number
+                   
+                    secNum=sum;	
+                
+                System.out.println(secNum);
+				}				
+	}
+
+}
---
+++ converstiontopositivefromnegative.java
@@ -0,0 +1,34 @@
+package com.java.basic.day1.assignments;
+
+public class converstiontopositivefromnegative {
+
+	/*1. Initialize an integer with a negative number
+	   like, int number = -40;
+	2. Check if the number is a negative number 
+	   Hint : any number that is lesser than zero is a negative number
+	3. If so, convert the number to a positive numer
+
+	4. Print as below
+	        "The number -40 is converted to 40" */
+	public static void main(String[] args) {
+		int number = -40;
+		
+		//single number print
+		if(number <0) {
+
+             int positive = number*-1;
+              System.out.println("The number"+number+"converted"+positive);
+
+		}
+//iterative 40 numbers conversion		
+	/*	for(number=-40;number<0;number++) {
+			int positive = number*-1;
+			if(positive>0) {
+				
+			System.out.println("The number "+number+"is converted to "+positive);
+		}
+	} */
+	}
+}
+
+
